ac4f8993fff27b5713d403bc5439da50c5ebc899
Nick Eubank <nick@nickeubank.com>
2019-12-31 17:12:14 -0500

    drop hawaii and alaska right from start

--- 10_code/00_prep_fipsconverter.py
+++ 10_code/00_prep_fipsconverter.py
@@ -1,30 +1,31 @@
 import geopandas as gpd
 import os
 
-os.chdir('/users/nick/github/us_ensembles')
-precincts = gpd.read_file("./00_source_data/2008_presidential_precinct_counts.shp")
+precincts = gpd.read_file("../00_source_data/votes/2008_presidential_precinct_counts.shp")
 
-
-precincts = precincts[['OBJECTID', 'P2008_D', 'P2008_R', 'STATE', 'COUNTY', 'geometry']]
 
 ##########
 # Get sequential to FIPS cross-walk
 # (SLURM likes to work with sequential counters)
 ##########
 
-state_codes = precincts.STATE.drop_duplicates().sort_values().reset_index(drop=True)
+state_codes = precincts.STATE.drop_duplicates().sort_values()
+
+# Now let's ignore Alaska and Hawaii
+state_codes = state_codes[~state_codes.isin(['02', '15'])].reset_index(drop=True)
+
 fips = state_codes.to_dict()
 
 # Couple stability tests
-assert len(state_codes) == 50
+assert len(state_codes) == len(fips)
 assert state_codes.loc[0] == '01'
-assert state_codes.loc[15] == '19'
-assert state_codes.loc[47] == '54'
+assert state_codes.loc[15] == '21'
+assert state_codes.loc[47] == '56'
 assert fips[0] == '01'
-assert fips[15] == '19'
-assert fips[47] == '54'
+assert fips[15] == '21'
+assert fips[47] == '56'
 
 import pickle
-with open('20_intermediate_files/sequential_to_fips.pickle', 'wb') as f:
+with open('../20_intermediate_files/sequential_to_fips.pickle', 'wb') as f:
     pickle.dump(fips, f)
     
