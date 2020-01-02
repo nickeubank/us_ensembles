130b5476c5d46628d4628eef1e54ceb98b786362
Nick Eubank <nick@nickeubank.com>
2020-01-02 10:12:48 -0500

    update pre-rpocess  code for FL

--- 10_code/10_prep_precincts.py
+++ 10_code/10_prep_precincts.py
@@ -18,108 +18,139 @@ state_fips_codes = sorted(list(pickle.load(open(f, "rb" )).values()))
 ###########
 
 # Precinct
-precincts = gpd.read_file("../00_source_data/votes/"
-                          "2008_presidential_precinct_counts.shp")
+master_precincts = gpd.read_file("../00_source_data/votes/"
+                                 "2008_presidential_precinct_counts.shp")
 
-precincts = precincts[['OBJECTID', 'P2008_D', 'P2008_R', 
-                       'STATE', 'COUNTY', 'geometry']]
+master_precincts = master_precincts[['OBJECTID', 'P2008_D', 'P2008_R', 
+                                     'STATE', 'COUNTY', 'geometry']]
 
 # District Maps
-districts = gpd.read_file('../00_source_data/legislative_districts/'
-                          'nhgis0190_shapefile_tl2014_us_cd114th_2014/'
-                          'US_cd114th_2014.shp')
+master_districts = gpd.read_file('../00_source_data/legislative_districts/'
+                                 'nhgis0190_shapefile_tl2014_us_cd114th_2014/'
+                                 'US_cd114th_2014.shp')
 
-districts = districts[['STATEFP', 'CD114FP', 'GEOID', 'geometry']]
+master_districts = master_districts[['STATEFP', 'CD114FP', 'GEOID', 'geometry']]
+
+
+thinned = [i for i in state_fips_codes if (i != "06") and (i != '12')]
 
 ########
 # Loopz
 ########
 
-for idx, state_fips in enumerate(state_fips_codes):
-    print(f'on step {idx}, fips {state_fips}')
+# to_fix = ['06', '12', '30']
 
-    ###########
-    # Bring in precincts, population, 
-    # and current districts
-    ###########
+for idx, state_fips in enumerate(to_fix):
     
-    precincts = precincts[precincts.STATE == state_fips].copy()
-    assert str(precincts.crs) == "{'init': 'epsg:3085'}"
+        print(f'on step {idx}, fips {state_fips}')
 
-    if len(precincts) == 0:
-        raise ValueError(f"no precincts for fips {state_fips}")
-    
-    # Topology problems
-    precincts.loc[~precincts.is_valid, 'geometry'] = precincts.loc[~precincts.is_valid, 'geometry'].buffer(0)
-    assert precincts.is_valid.all()
+        ###########
+        # Bring in precincts, population, 
+        # and current districts
+        ###########
 
-    # Census blocks
-    f_blocks = f'../00_source_data/census_blocks/tabblock2010_{state_fips}'\
-               f'_pophu/tabblock2010_{state_fips}_pophu.shp'
+        precincts = master_precincts[master_precincts.STATE == state_fips].copy()
 
-    census_blocks = gpd.read_file(f_blocks)
+        if len(precincts) == 0:
+            raise ValueError(f"no precincts for fips {state_fips}")
 
-    assert census_blocks.is_valid.all()
+        # Topology problems
+        assert str(precincts.crs) == "{'init': 'epsg:3085'}"
+        precincts.loc[~precincts.is_valid, 'geometry'] = precincts.loc[~precincts.is_valid, 'geometry'].buffer(0)
+        assert precincts.is_valid.all()
 
-    # Legislative Districts
-    districts = districts[districts['STATEFP'] == state_fips].copy()
+        # Census blocks
+        f_blocks = f'../00_source_data/census_blocks/tabblock2010_{state_fips}'\
+                   f'_pophu/tabblock2010_{state_fips}_pophu.shp'
 
-    # Topology problems
-    districts.loc[~districts.is_valid, 'geometry'] = districts.loc[~districts.is_valid, 'geometry'].buffer(0)
-    assert districts.is_valid.all()
+        census_blocks = gpd.read_file(f_blocks)
+
+        assert census_blocks.is_valid.all()
+
+        # Legislative Districts
+        districts = master_districts[master_districts['STATEFP'] == state_fips].copy()
+
+        # Topology problems
+        districts.loc[~districts.is_valid, 'geometry'] = districts.loc[~districts.is_valid, 'geometry'].buffer(0)
+        assert districts.is_valid.all()
 
 
-    #########
-    # Common reprojection
-    #########
+        #########
+        # Common reprojection
+        #########
 
-    crs = 'EPSG:3085'
+        crs = 'EPSG:3085'
 
-    census_blocks = census_blocks.to_crs(crs)
-    precincts = precincts.to_crs(crs)
-    districts = districts.to_crs(crs)
+        census_blocks = census_blocks.to_crs(crs)
+        precincts = precincts.to_crs(crs)
+        districts = districts.to_crs(crs)
 
-    ###########
-    # Put population and district
-    # into precincts
-    ###########
+        ###########
+        # Put population and district
+        # into precincts
+        ###########
 
-    import maup
+        import maup
 
-    # Districts
-    district_assignment = maup.assign(precincts, districts)
-    precincts["district"] = district_assignment
+        # Districts
+        district_assignment = maup.assign(precincts, districts)
+        precincts["district"] = district_assignment
 
-    # Population
-    assignment = maup.assign(census_blocks, precincts)
-    precincts['population'] = census_blocks['POP10'].groupby(assignment).sum()
-    precincts['population'].head()
-
-    ###########
-    # Fill gaps and overlaps
-    ###########
-    #precincts_no_overlaps = maup.resolve_overlaps(precincts)
-    #precincts_no_overlaps_or_gaps = maup.resolve_gaps(precincts_no_overlaps)
-
-    ###########
-    # Deal with NAs
-    ###########
-
-    import pandas as pd
-
-    missing = pd.isnull(precincts['district']) | pd.isnull(precincts['population'])
-
-    if (missing.sum() / len(precincts)) > 0.005:
-        raise ValueError("missing district or population data for more than 5% of precincts"
-                         f"in state fips {state_fips}")
+        # Population
         
-    precincts = precincts[~missing]
+        # have to drop census blocks that fully miss precincts (very rare, but happens.)
+        # e.g. everglade reserve in FL
+        
+        if state_fips == '12':
+            
+            # OK, this is super hacky because geopandas is so 
+            # bad at merging. But this will work. 
+            
+            precincts['temp'] = 1
+            precincts_unary = precincts[['temp', 'geometry']].dissolve(by='temp')
+            precincts_unary = precincts_unary.simplify(0.05)
+            precincts_unary = gpd.GeoDataFrame(precincts_unary, geometry=0)
+            precincts_unary.crs = precincts.crs
+            
+            pre_merge_census_len = len(census_blocks)
+            census_blocks = gpd.sjoin(census_blocks, precincts_unary,
+                                      how='inner', op='intersects')
+        
+        assert len(intersecting_census_blocks) <= len(census_blocks)
+        assignment = maup.assign(intersecting_census_blocks, precincts)
+        
+        precincts['population'] = intersecting_census_blocks['POP10'].groupby(assignment).sum()
+        precincts['population'].head()
 
-    ###########
-    # Save
-    ###########
+        assignment = maup.assign(census_blocks, precincts)
+        precincts['population'] = census_blocks['POP10'].groupby(assignment).sum()
+        precincts['population'].head()
 
-    f = f'../20_intermediate_files/pre_processed_precinct_maps/precincts_{state_fips}.shp'
-    #precincts_no_overlaps_or_gaps.to_file(f)
-    precincts.to_file(f)
-    print(f'finished {state_fips}')
+        
+        ###########
+        # Fill gaps and overlaps
+        ###########
+        #precincts_no_overlaps = maup.resolve_overlaps(precincts)
+        #precincts_no_overlaps_or_gaps = maup.resolve_gaps(precincts_no_overlaps)
+
+        ###########
+        # Deal with NAs
+        ###########
+
+        import pandas as pd
+
+        missing = pd.isnull(precincts['district']) | pd.isnull(precincts['population'])
+
+        if (missing.sum() / len(precincts)) > 0.005:
+            raise ValueError("missing district or population data for more than 5% of precincts"
+                             f"in state fips {state_fips}")
+
+        precincts = precincts[~missing]
+
+        ###########
+        # Save
+        ###########
+
+        f = f'../20_intermediate_files/pre_processed_precinct_maps/precincts_{state_fips}.shp'
+        precincts.to_file(f)
+        print(f'finished {state_fips}')
--- 10_code/10_prep_precincts_parallel.py
+++ 10_code/10_prep_precincts_parallel.py
@@ -12,93 +12,106 @@ import pickle
 def setup_state_by_fips(bundle):
 
     # Unbundle args. Can only pass 1 to joblib. 
-    fips, precincts, districts = bundle
-    
-    ###########
-    # Bring in precincts, population, 
-    # and current districts
-    ###########
-    
-    precincts = precincts[precincts.STATE == state_fips].copy()
-    assert str(precincts.crs) == "{'init': 'epsg:3085'}"
+    state_fips, precincts, districts = bundle
 
-    if len(precincts) == 0:
-        raise ValueError(f"no precincts for fips {state_fips}")
-    
-    # Topology problems
-    precincts.loc[~precincts.is_valid, 'geometry'] = precincts.loc[~precincts.is_valid, 'geometry'].buffer(0)
-    assert precincts.is_valid.all()
+    try: 
+        ###########
+        # Bring in precincts, population, 
+        # and current districts
+        ###########
 
-    # Census blocks
-    f_blocks = f'../00_source_data/census_blocks/tabblock2010_{state_fips}'\
-               f'_pophu/tabblock2010_{state_fips}_pophu.shp'
+        precincts = master_precincts[master_precincts.STATE == state_fips].copy()
 
-    census_blocks = gpd.read_file(f_blocks)
+        if len(precincts) == 0:
+            raise ValueError(f"no precincts for fips {state_fips}")
 
-    assert census_blocks.is_valid.all()
+        assert str(precincts.crs) == "{'init': 'epsg:3085'}"
 
-    # Legislative Districts
-    districts = districts[districts['STATEFP'] == state_fips].copy()
+        # Topology problems
+        precincts.loc[~precincts.is_valid, 'geometry'] = precincts.loc[~precincts.is_valid, 'geometry'].buffer(0)
+        assert precincts.is_valid.all()
 
-    # Topology problems
-    districts.loc[~districts.is_valid, 'geometry'] = districts.loc[~districts.is_valid, 'geometry'].buffer(0)
-    assert districts.is_valid.all()
+        # Census blocks
+        f_blocks = f'../00_source_data/census_blocks/tabblock2010_{state_fips}'\
+                   f'_pophu/tabblock2010_{state_fips}_pophu.shp'
+
+        census_blocks = gpd.read_file(f_blocks)
+
+        assert census_blocks.is_valid.all()
+
+        # Legislative Districts
+        districts = master_districts[master_districts['STATEFP'] == state_fips].copy()
+
+        # Topology problems
+        districts.loc[~districts.is_valid, 'geometry'] = districts.loc[~districts.is_valid, 'geometry'].buffer(0)
+        assert districts.is_valid.all()
 
 
-    #########
-    # Common reprojection
-    #########
+        #########
+        # Common reprojection
+        #########
 
-    crs = 'EPSG:3085'
+        crs = 'EPSG:3085'
 
-    census_blocks = census_blocks.to_crs(crs)
-    precincts = precincts.to_crs(crs)
-    districts = districts.to_crs(crs)
+        census_blocks = census_blocks.to_crs(crs)
+        precincts = precincts.to_crs(crs)
+        districts = districts.to_crs(crs)
 
-    ###########
-    # Put population and district
-    # into precincts
-    ###########
+        ###########
+        # Put population and district
+        # into precincts
+        ###########
 
-    import maup
+        import maup
 
-    # Districts
-    district_assignment = maup.assign(precincts, districts)
-    precincts["district"] = district_assignment
+        # Districts
+        district_assignment = maup.assign(precincts, districts)
+        precincts["district"] = district_assignment
 
-    # Population
-    assignment = maup.assign(census_blocks, precincts)
-    precincts['population'] = census_blocks['POP10'].groupby(assignment).sum()
-    precincts['population'].head()
-
-    ###########
-    # Fill gaps and overlaps
-    ###########
-    #precincts_no_overlaps = maup.resolve_overlaps(precincts)
-    #precincts_no_overlaps_or_gaps = maup.resolve_gaps(precincts_no_overlaps)
-
-    ###########
-    # Deal with NAs
-    ###########
-
-    import pandas as pd
-
-    missing = pd.isnull(precincts['district']) | pd.isnull(precincts['population'])
-
-    if (missing.sum() / len(precincts)) > 0.005:
-        raise ValueError("missing district or population data for more than 5% of precincts"
-                         f"in state fips {state_fips}")
+        # Population 
         
-    precincts = precincts[~missing]
+        # have to drop census blocks that fully miss precincts (very rare, but happens.)
+        # e.g. everglade reserve in FL
+        precincts_unary = precincts.geometry.unary_union
+        intersecting_census_blocks = gpd.sjoin(census_blocks, precincts[['district', 'geometry']], 
+                                               how='inner', op='intersects')
+        intersecting_census_blocks = intersecting_census_blocks.drop('district', axis='columns').drop_duplicates()
+        assignment = maup.assign(intersecting_census_blocks, precincts)
+        precincts['population'] = intersecting_census_blocks['POP10'].groupby(assignment).sum()
+        precincts['population'].head()
 
-    ###########
-    # Save
-    ###########
+        ###########
+        # Fill gaps and overlaps
+        ###########
+        #precincts_no_overlaps = maup.resolve_overlaps(precincts)
+        #precincts_no_overlaps_or_gaps = maup.resolve_gaps(precincts_no_overlaps)
 
-    f = f'../20_intermediate_files/pre_processed_precinct_maps/precincts_{state_fips}.shp'
-    #precincts_no_overlaps_or_gaps.to_file(f)
-    precincts.to_file(f)
-    print(f'finished {state_fips}')
+        ###########
+        # Deal with NAs
+        ###########
+
+        import pandas as pd
+
+        missing = pd.isnull(precincts['district']) | pd.isnull(precincts['population'])
+
+        if (missing.sum() / len(precincts)) > 0.005:
+            raise ValueError("missing district or population data for more than 5% of precincts"
+                             f"in state fips {state_fips}")
+
+        precincts = precincts[~missing]
+
+        ###########
+        # Save
+        ###########
+
+        f = f'../20_intermediate_files/pre_processed_precinct_maps/precincts_{state_fips}.shp'
+        #precincts_no_overlaps_or_gaps.to_file(f)
+        precincts.to_file(f)
+        print(f'finished {state_fips}')
+        return None
+    except: 
+        print(f'failed {state_fips}')
+        return state_fips
 
 #######
 # Actual run
@@ -107,30 +120,31 @@ def setup_state_by_fips(bundle):
 from joblib import Parallel, delayed
 
 f='../20_intermediate_files/sequential_to_fips.pickle'
-state_fips_codes = pickle.load(open(f, "rb" )).values()
+state_fips_codes = sorted(list(pickle.load(open(f, "rb" )).values()))
 
 ###########
 # Loads used for all passes
 ###########
 
 # Precinct
-precincts = gpd.read_file("../00_source_data/votes/"
-                          "2008_presidential_precinct_counts.shp")
+master_precincts = gpd.read_file("../00_source_data/votes/"
+                                 "2008_presidential_precinct_counts.shp")
 
-precincts = precincts[['OBJECTID', 'P2008_D', 'P2008_R', 
-                       'STATE', 'COUNTY', 'geometry']]
+master_precincts = master_precincts[['OBJECTID', 'P2008_D', 'P2008_R', 
+                                     'STATE', 'COUNTY', 'geometry']]
 
 # District Maps
-districts = gpd.read_file('../00_source_data/legislative_districts/'
-                          'nhgis0190_shapefile_tl2014_us_cd114th_2014/'
-                          'US_cd114th_2014.shp')
+master_districts = gpd.read_file('../00_source_data/legislative_districts/'
+                                 'nhgis0190_shapefile_tl2014_us_cd114th_2014/'
+                                 'US_cd114th_2014.shp')
 
-districts = districts[['STATEFP', 'CD114FP', 'GEOID', 'geometry']]
+master_districts = master_districts[['STATEFP', 'CD114FP', 'GEOID', 'geometry']]
 
 #######
 # And... EXECUTE!
 #######
 
-results = (Parallel(n_jobs=10, verbose=5)
+results = (Parallel(n_jobs=10, verbose=5, backend='multiprocessing')
            (delayed(setup_state_by_fips)
-           ((fips, precincts, districts)) for fips in state_fips_codes)
+           ((fips, master_precincts, master_districts)) for fips in state_fips_codes)
+          )
