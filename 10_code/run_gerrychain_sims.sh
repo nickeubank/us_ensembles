b3ff93303d8b0a85dddc7db4a56573e282ddc330
Nick Eubank <nick@nickeubank.com>
2020-01-02 12:49:47 -0500

    add slurm submission

---
+++ 10_code/run_gerrychain_sims.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --mail-user=nick@nickeubank.com  # email address
+#SBATCH --mail-type=ALL  # Alerts sent when job begins, ends, or aborts
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=10G
+#SBATCH --job-name=gerrychain
+#SBATCH --array=1-2
+#SBATCH --time=00-00:20:00  # Wall Clock time (dd-hh:mm:ss) [max of 14 days]
+#SBATCH --output=slurm_reports/slurm_reports_%A_%a.output  # output and error messages go to this file
+
+export STATE=$SLURM_ARRAY_TASK_ID
+
+python 20_run_simulations.py
