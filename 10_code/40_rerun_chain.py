3d374242cdd59f95c0afea3daceb22882445815a
Daryl DeFord <daryl.r.deford@gmail.com>
2020-01-23 14:46:26 -0500

    correct state names

--- 10_code/40_rerun_chain.py
+++ 10_code/40_rerun_chain.py
@@ -29,6 +29,18 @@ import ast
 #with open(newdir + "init.txt", "w") as f:
 #    f.write("Created Folder")
 
+state_names={"02":"Alaska","01":"Alabama","05":"Arkansas","04":"Arizona",
+"06":"California","08":"Colorado","09":"Connecticut","10":"Delaware",
+"12":"Florida","13":"Georgia","66":"Guam","15":"Hawaii","19":"Iowa",
+"16":"Idaho","17":"Illinois","18":"Indiana","20":"Kansas","21":"Kentucky",
+"22":"Louisiana","25":"Massachusetts","24":"Maryland","23":"Maine","26":"Michigan",
+"27":"Minnesota","29":"Missouri","28":"Mississippi","30":"Montana",
+"37":"North_Carolina","38":"North_Dakota","31":"Nebraska","33":"New_Hampshire",
+"34":"New_Jersey","35":"New_Mexico","32":"Nevada","36":"New_York","39":"Ohio",
+"40":"Oklahoma","41":"Oregon","42":"Pennsylvania","72":"Puerto_Rico",
+"44":"Rhode_Island","45":"South_Carolina","46":"South_Dakota","47":"Tenessee",
+"48":"Texas","49":"Utah","51":"Virginia","50":"Vermont","53":"Washington",
+"55":"Wisconsin","54":"West_Virginia","56":"Wyoming"}
 
 
 num_elections = 1
@@ -37,7 +49,54 @@ election_names = ["PRES2008"]
 
 election_columns = [["P2008_D",  "P2008_R"]]
 
-fips_list = ['01']
+fips_list = [
+        '01',
+        '04',
+        '05',
+        '06',
+        '08',
+        '09',
+        '10',
+        '11',
+        '12',
+        '13',
+        '16',
+        '17',
+        '18',
+        '19',
+        '20',
+        '21',
+        '22',
+        '23',
+        '24',
+        '25',
+        '26',
+        '27',
+        '28',
+        '29',
+        '31',
+        '32',
+        '33',
+        '34',
+        '35',
+        '36',
+        '37',
+        '38',
+        '39',
+        '40',
+        '42',
+        '44',
+        '45',
+        '46',
+        '47',
+        '48',
+        '49',
+        '50',
+        '51',
+        '53',
+        '54',
+        '55',
+        '56']
 
 plan_name = "Enacted"
 
@@ -61,7 +120,7 @@ for state_fips in fips_list:
     #Point initialization happens here
     #check for crs matching!
     state_precincts = gpd.read_file(f"../20_intermediate_files/pre_processed_precinct_maps/precincts_{state_fips}.shp")
-    state_points = gpd.read_file("../../../Dropbox/dislocation_intermediate_files/60_voter_knn_scores/shapefiles/Alabama_USHouse.shp") # THIS FILEANAME isn't quite right - need to check format values. 
+    state_points = gpd.read_file("../../../Dropbox/dislocation_intermediate_files/60_voter_knn_scores/shapefiles/{state_names[state_fips]}_USHouse.shp") # THIS FILEANAME isn't quite right - need to check format values. 
     print("loaded precincts/points")
 
     print(state_precincts.crs)
