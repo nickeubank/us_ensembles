15763ba2dd00700fc2e39cb30995388b75061078
Nick Eubank <nick@nickeubank.com>
2020-02-15 11:03:37 -0500

    split building graphs and seeding

--- 10_code/15_build_graph_parallel.py
+++ 10_code/15_build_graph_parallel.py
@@ -118,29 +118,9 @@ def build_graphs(state_fips):
 
     print(f'done with connections for {state_fips}', flush=True)
 
-    graph.to_json(f'../20_intermediate_files/precinct_graphs/'
-                  f'precinct_graphs_{state_fips}_PRESEED.json')
+    graph.to_json(f'../20_intermediate_files/precinct_graphs/preseed/'
+                  f'precinct_graphs_{state_fips}.json')
     
-    for new_seed in range(3):
-
-        if state_fips not in ['12', '06']:
-            cddict =  recursive_tree_part(graph, range(num_districts), 
-                                          totpop / num_districts, "population", .02, 1)
-        else: 
-            cddict =  recursive_tree_part(graph, range(num_districts), 
-                                          totpop / num_districts, "population", .05, 1)
-
-            
-        pos = {node:(float(graph.nodes[node]['C_X']), 
-                     float(graph.nodes[node]['C_Y'])) for node in graph.nodes}
-
-        for node in graph.nodes():
-            graph.nodes[node]['New_Seed'] = cddict[node]
-            graph.nodes[node]['pos'] = pos[node]
-
-        graph.to_json(f'../20_intermediate_files/precinct_graphs/'
-                      f'precinct_graphs_{state_fips}_seed{new_seed}.json')
-
     print(f'all done with {state_fips}', flush=True)
         
 #######
